97bd09d44f482fd061771c28b7ab3a908795108e
OlgaVorob <vorobieva1c@gmail.com>
2022-07-27 01:11:54 +0300

    Numbers

---
+++ 01_base.js
@@ -0,0 +1,155 @@
+////1. переменные (используем стайл cameCase)
+
+//var name = 'Olga' deprecated - устарело
+// const myName = 'Olga' //const не меняем. const предпочтительнее чем let если не меняем
+// let age = 'age?'
+
+// age = 30
+
+// console.log(age + 10)
+
+// //имя переменной:
+// const _myConst = 1
+// const _ = 1
+// const $ = 1
+//const 5myConst = 1 //err
+
+////2. Мутирование. Concatenation - (склеивание строк по сути)
+// console.log('The name is ' + myName + ', and the age is ' + age)
+// //у перем age, кот число закулисами вызывается age.toString()
+// console.log('The name is ' + myName + ', and the age is ' + age.toString())
+
+// const lastName = prompt('Insert lastName: ')
+// alert(myName + ' ' + lastName)
+
+////3. Операторы
+// let currentYear = 2022
+// const a = 10
+// const b = 5
+// let c = 32
+
+// c += a //c = c + a
+
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// //console.log(currentYear++) сначала выведет, потом инкрементирует
+// console.log(++currentYear)
+// console.log(currentYear)
+// console.log(--currentYear)
+// console.log(c)
+
+////4. Типы данных
+const isProgrammer = true
+const name = 'Olga'
+const age = 34
+let x
+const y = null
+
+console.log(typeof isProgrammer) //boolean
+console.log(typeof name) //string
+console.log(typeof age) //number
+console.log(typeof x) //undefined
+console.log(typeof y) //тут какой-то баг, выводит как object, хотя должен null
+
+//5. Приоритет операторов
+const fullAge = 34
+const birthYear = 1988
+const currentYear = 2022
+const isFullAge = currentYear - birthYear >= fullAge
+console.log(isFullAge)
+
+//6. условные операторы
+const courseStatus = 'pending' //ready, fail, pending
+
+if (courseStatus === 'ready') {
+    console.log('The course is ready')
+} else if (courseStatus === 'pending') {
+    console.log('The course is in progress')
+} else {
+    console.log('The course are not defined')
+}
+
+const num1 = 42
+const num2 = '42'
+console.log(num1 == num2) //true // == оператор приводит к типу число
+console.log(num1 === num2) //false // не приводит
+
+const isReady = true
+// if (isReady) {
+//     console.log('All is ready!')
+// }
+
+isReady ? console.log('All is ready!') : console.log('Nothing is ready!') //ternary operator
+
+//7. Булевая логика
+
+//8. Функции
+
+function calculateAge(year) {
+    return 2022 - year
+}
+
+//console.log(calculateAge(1988))
+
+function logInfoAbout(name, year) {
+    const age = calculateAge(year)
+    if (age > 0) {
+    console.log('Человек по имени ' + name + ' сейчас имеет возраст ' + age)
+    } else {
+        console.log('Введенный год больше текущего!')
+    }
+}
+
+logInfoAbout('Tom', 1990)
+logInfoAbout('Eva', 2030)
+
+//9. Array
+//const cars = new Array('mazda', 'mercedes', 'ford') - под капотом
+const cars = ['mazda', 'mercedes', 'ford'] //js сам преобразует в выражение выше
+
+console.log(cars.length)
+console.log(cars[0])
+console.log(cars[3])
+
+cars[0] = 'Porsche'
+cars[cars.length] = 'mazda' 
+console.log(cars)
+
+//10. Циклы
+const bestCars = ['mazda', 'mercedes', 'ford']
+
+for (let i = 0; i < bestCars.length; i++) {
+    const car = bestCars[i] //??? let
+    console.log(car)
+}
+//в последней версии JS появился цикл forof
+for (let car of bestCars) {
+    console.log(car)
+}
+
+// 11.Объекты (что-то вроде группировки значений, но в отличии от массива создаем различные структуры)
+
+//const person = new Object({})   //под капотом
+const person = {        //перечисляем свойства как ключ-значение
+    firstName: 'Tom',  
+    lastName: 'Petrov',
+    year: 1990,
+    languages: ['Ru', 'En', 'De'],
+    hasWife: false,
+    greet: function() {
+        console.log('greet for person')
+    } //Это метод (функция, определеная в контексте объекта)
+}
+
+console.log(person.firstName)    //1й способ обращения к свойству
+console.log(person['firstName']) //2й способ обращения к свойству
+const key = 'firstName'          
+console.log(person[key])         //3й способ обращения к свойству
+person.greet() //обращение к методу
+
+person.hasWife = true
+person.isProgrammer = true
+console.log(person)
--- app.js
+++ app.js
@@ -1,149 +1,24 @@
-////1. переменные (используем стайл cameCase)
+//Numbers
+const numInt = 42
+const numFloat = 42.2
+const numPow = 10e3   //10000
 
-//var name = 'Olga' deprecated - устарело
-// const myName = 'Olga' //const не меняем. const предпочтительнее чем let если не меняем
-// let age = 'age?'
-
-// age = 30
-
-// console.log(age + 10)
-
-// //имя переменной:
-// const _myConst = 1
-// const _ = 1
-// const $ = 1
-//const 5myConst = 1 //err
-
-////2. Мутирование. Concatenation - (склеивание строк по сути)
-// console.log('The name is ' + myName + ', and the age is ' + age)
-// //у перем age, кот число закулисами вызывается age.toString()
-// console.log('The name is ' + myName + ', and the age is ' + age.toString())
-
-// const lastName = prompt('Insert lastName: ')
-// alert(myName + ' ' + lastName)
-
-////3. Операторы
-// let currentYear = 2022
-// const a = 10
-// const b = 5
-// let c = 32
-
-// c += a //c = c + a
+console.log('Number.MAX_SAFE_INTEGER', Number.MAX_SAFE_INTEGER)   //9007199254740991
+console.log('Math.pow 53', Math.pow(2, 53) - 1)
+console.log('Number.MIN_SAFE_INTEGER', Number.MIN_SAFE_INTEGER)   //-9007199254740991
+console.log('Number.MAX_VALUE', Number.MAX_VALUE)                 //1.7976931348623157e+308
+console.log('Number.MIN_VALUE', Number.MIN_VALUE)                 //5e-324
+console.log('Number.POSITIVE_INFINITY', Number.POSITIVE_INFINITY) //Infinity
+console.log('Number.NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY) //-Infinity   //number!
+console.log('2 / 0', 2 / 0)                                       //Infinity    //number!
+console.log(isFinite(Infinity)) //false  = Number.isFinite(Infinity)
+console.log(isFinite(42))       //true
 
 
-// console.log(a + b)
-// console.log(a - b)
-// console.log(a * b)
-// console.log(a / b)
-// //console.log(currentYear++) сначала выведет, потом инкрементирует
-// console.log(++currentYear)
-// console.log(currentYear)
-// console.log(--currentYear)
-// console.log(c)
-
-////4. Типы данных
-const isProgrammer = true
-const name = 'Olga'
-const age = 34
-let x
-const y = null
-
-console.log(typeof isProgrammer) //boolean
-console.log(typeof name) //string
-console.log(typeof age) //number
-console.log(typeof x) //undefined
-console.log(typeof y) //тут какой-то баг, выводит как object, хотя должен null
-
-//5. Приоритет операторов
-const fullAge = 34
-const birthYear = 1988
-const currentYear = 2022
-const isFullAge = currentYear - birthYear >= fullAge
-console.log(isFullAge)
-
-//6. условные операторы
-const courseStatus = 'pending' //ready, fail, pending
-
-if (courseStatus === 'ready') {
-    console.log('The course is ready')
-} else if (courseStatus === 'pending') {
-    console.log('The course is in progress')
-} else {
-    console.log('The course are not defined')
-}
-
-const num1 = 42
-const num2 = '42'
-console.log(num1 == num2) //true // == оператор приводит к типу число
-console.log(num1 === num2) //false // не приводит
-
-const isReady = true
-// if (isReady) {
-//     console.log('All is ready!')
-// }
-
-isReady ? console.log('All is ready!') : console.log('Nothing is ready!') //ternary operator
-
-//7. Булевая логика
-
-//8. Функции
-
-function calculateAge(year) {
-    return 2022 - year
-}
-
-//console.log(calculateAge(1988))
-
-function logInfoAbout(name, year) {
-    const age = calculateAge(year)
-    if (age > 0) {
-    console.log('Человек по имени ' + name + ' сейчас имеет возраст ' + age)
-    } else {
-        console.log('Введенный год больше текущего!')
-    }
-}
-
-logInfoAbout('Tom', 1990)
-logInfoAbout('Eva', 2030)
-
-//9. Array
-//const cars = new Array('mazda', 'mercedes', 'ford') - под капотом
-const cars = ['mazda', 'mercedes', 'ford'] //js сам преобразует в выражение выше
-
-console.log(cars.length)
-console.log(cars[0])
-console.log(cars[3])
-
-cars[0] = 'Porsche'
-cars[cars.length] = 'mazda' 
-console.log(cars)
-
-//10. Циклы
-const bestCars = ['mazda', 'mercedes', 'ford']
-
-for (let i = 0; i < bestCars.length; i++) {
-    const car = bestCars[i] //??? let
-    console.log(car)
-}
-//в последней версии JS появился цикл forof
-for (let car of bestCars) {
-    console.log(car)
-}
-
-// 11.Объекты (что-то вроде группировки значений, но в отличии от массива создаем различные структуры)
-
-//const person = new Object({})   //под капотом
-const person = {        //перечисляем свойства как ключ-значение
-    firstName: 'Tom',  
-    lastName: 'Petrov',
-    year: 1990,
-    languages: ['Ru', 'En', 'De'],
-    hasWife: false,
-    greet: function() {
-        console.log('greet for person')
-    } //Это метод (функция, определеная в контексте объекта)
-}
-//способы обращения к свойствам и методам
-console.log(person.firstName) //1.
-
-person.greet()
+console.log(Number.NaN)                                           //NaN         //number! хотя переводится как 'Not a number'
+console.log(NaN)   //у Number.NaN есть ключевое слово NaN
+console.log(typeof NaN) //'number'
+const wierd = 2 / undefined
+console.log(wierd)   //NaN
+//проверка на isNaN:
+console.log(Number.isNaN(wierd))  //true  = isNaN(wierd)
